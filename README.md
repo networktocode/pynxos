bb919eeba5c883fd51b6996802fb53aa3fd99998
jathanism <jathan@gmail.com>
2019-04-22 08:46:22 -0700

    Add license, readme, and bump version to 0.0.5

---
+++ README.md
@@ -0,0 +1,3 @@
+# pynxos
+
+A library for managing Cisco NX-OS devices through NX-API.
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(name='pynxos',
-      version='0.0.3',
+      version='0.0.5',
       packages=find_packages(),
       description='A library for managing Cisco NX-OS devices through NX-API.',
       author='Network To Code',
       author_email='ntc@networktocode.com',
       url='https://github.com/networktocode/pynxos/',
-      download_url='https://github.com/networktocode/pynxos/tarball/master',
+      license='Apache',
       install_requires=['requests>=2.7.0', 'future', 'scp']
       )
